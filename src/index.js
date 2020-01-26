a9f3e052ebd79ae9504150f270741f4742193d1c
Hossain Saddam <hossaincrr7@gmail.com>
2020-01-26 08:39:06 +0600

    WIP hero load animation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 // import styles
 import './styles/main.scss';
 
-import { gsap, ScrollToPlugin } from "gsap/all";
-gsap.registerPlugin(ScrollToPlugin); 
+import {
+  gsap,
+  ScrollToPlugin
+} from "gsap/all";
+gsap.registerPlugin(ScrollToPlugin);
+
+import ScrollMagic from 'scrollmagic'
 
 
-
-window.addEventListener('scroll',(e) => {
-  console.log(e);
-  
+gsap.to(".o-hero", {
+  height: `100%`,
+  duration: 1
+});
+gsap.to(".o-hero__banner", {
+  top: '-900px',
+  left: '-2500px',
+  transform: `scale(1)`,
+  duration: 1
 })
