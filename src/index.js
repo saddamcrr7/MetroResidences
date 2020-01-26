f754280f47ec0e26528d092f29ca47d203c905d0
Hossain Saddam <hossaincrr7@gmail.com>
2020-01-26 22:29:41 +0600

    Update

--- src/index.js
+++ src/index.js
@@ -3,20 +3,31 @@ import './styles/main.scss';
 
 import {
   gsap,
-  ScrollToPlugin
+  ScrollToPlugin,
+  TweenMax, TimelineMax,
 } from "gsap/all";
-gsap.registerPlugin(ScrollToPlugin);
+gsap.registerPlugin(ScrollToPlugin,TimelineMax, TweenMax);
 
 import ScrollMagic from 'scrollmagic'
 
+const heroLoad = () => {
+  const tl = gsap.timeline()
 
-gsap.to(".o-hero", {
-  height: `100%`,
-  duration: 1
-});
-gsap.to(".o-hero__banner", {
-  top: '-900px',
-  left: '-2500px',
-  transform: `scale(1)`,
-  duration: 1
-})
+  tl.to(".o-hero__container", {
+    height: `100%`,
+    duration: 1,
+    ease: "power2.out",
+  }).to(".o-hero__title", {duration:0.5, opacity: 1,});
+
+  gsap.to(".o-hero__banner", {
+    top: '-900px',
+    left: '-2500px',
+    transform: `scale(1)`,
+    duration: 1,
+    ease: "power2.out",
+  })
+}
+
+heroLoad()
+
+var controller = new ScrollMagic.Controller();
