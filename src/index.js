2486b1077f9c3684e725b78a5f31b86b0601c6fe
Hossain Saddam <hossaincrr7@gmail.com>
2020-01-26 07:39:11 +0600

    WIP Hero section

--- src/index.js
+++ src/index.js
@@ -3,3 +3,10 @@ import './styles/main.scss';
 
 import { gsap, ScrollToPlugin } from "gsap/all";
 gsap.registerPlugin(ScrollToPlugin); 
+
+
+
+window.addEventListener('scroll',(e) => {
+  console.log(e);
+  
+})
