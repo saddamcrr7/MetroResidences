6e66feba292de989978f19c942422a7d476b3e10
Hossain Saddam <hossaincrr7@gmail.com>
2020-01-27 18:12:29 +0600

    update header

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './styles/main.scss';
 
 const heroLoad = () => {
   TweenMax.to(".o-hero__container", 1, {height: `100%`,ease: "power2.out",})
-  TweenMax.to(".o-hero__banner", 1, {top: '-900px',left: '-2500px', transform: `scale(1)`, ease: "power2.out",})
+  TweenMax.to(".o-hero__banner", 1, {top: '-900px',left: '-2600px', transform: `scale(1.4)`, ease: "power2.out",})
 }
 
 heroLoad()
@@ -14,23 +14,20 @@ const controller = new ScrollMagic.Controller();
 
 const tl = new TimelineMax();
 tl.staggerFrom(".o-hero__title", 1, {opacity: 0, })
-.to(".o-hero__banner", 1, {top: '-400px',left: '-1500px'}, '-=1').to(".o-hero__title", 1, {top: "-100%", })
+.to(".o-hero__banner", 1, {top: '-400px',left: '-1600px'}, '-=1')
 
 const tl1 = new TimelineMax();
 tl1.to(".o-hero__title", 1, {top: "-100%", })
 .to(".o-hero__sub", 1, {top: '50%', },'-=1')
-.to(".o-hero__banner", 1, {top: '-3000px',left: '-1500px'}, '-=1')
+.to(".o-hero__banner", 1, {top: '-1500px',left: '-1600px'}, '-=1')
 
 const scene = new ScrollMagic.Scene({
-  triggerElement: "#stage-1",
+  triggerElement: "#o-stage-1",
   triggerHook: 0,
-  duration: "50%",
-
 }).setTween(tl).addTo(controller);
 
 const scene2 = new ScrollMagic.Scene({
-  triggerElement: "#stage-2",
+  triggerElement: "#o-stage-2",
   triggerHook: 0,
-  duration: "20%",
 
 }).setTween(tl1).addTo(controller);
