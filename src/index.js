47300794b73cde3c80d5c3cd709ac5734cc50df0
Hossain Saddam <hossaincrr7@gmail.com>
2020-02-12 05:36:15 +0600

    WIP facilities section

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import './styles/main.scss';
 
 import heroSction from './scripts/blocks/hero-section'
 import roomsScetion from './scripts/blocks/rooms-section'
+import facilitiesSection from './scripts/blocks/facilities-section'
 
 heroSction()
 roomsScetion()
+facilitiesSection()
