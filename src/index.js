b2473de22c872fc2141af7db563a7a386f10acff
Hossain Saddam <hossaincrr7@gmail.com>
2020-02-13 08:44:57 +0600

    WIP location section

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ import heroSction from './scripts/blocks/hero-section'
 import roomsScetion from './scripts/blocks/rooms-section'
 import facilitiesSection from './scripts/blocks/facilities-section'
 import securitySection from './scripts/blocks/security-section'
+import locationSction from './scripts/blocks/location-section'
 
 heroSction()
 roomsScetion()
 facilitiesSection()
 securitySection()
+locationSction()
---
+++ src/scripts/blocks/location-section.js
@@ -0,0 +1,36 @@
+import initMap from '../components/map'
+import controller from '../util/ScrollMagic'
+
+const locationSction = () => {
+  window.initMap = initMap;
+
+  const tl11 = new TimelineMax();
+  tl11.to('.o-location', 0.4, {
+    opacity: 1,
+    pointerEvents: 'auto'
+  }).to('.o-location__map-container', 0.2, {
+    width: 760,
+    height: 760,
+    display: 'block'
+  }, '-=0.1')
+
+  const scene11 = new ScrollMagic.Scene({
+    triggerElement: "#o-stage-11",
+    triggerHook: 0,
+  }).setTween(tl11).addTo(controller);
+
+
+  const tl12 = new TimelineMax();
+  tl12.to('.o-location__map-container', 0.5, {
+    width: '100%',
+    height: '100%',
+    borderRadius: 0
+  })
+
+  const scene12 = new ScrollMagic.Scene({
+    triggerElement: "#o-stage-12",
+    triggerHook: 0,
+  }).setTween(tl12).addTo(controller);
+}
+
+export default locationSction
