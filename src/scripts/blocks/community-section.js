d86a0289a91c1447b20b6fc07504e2bae384d8b7
Hossain Saddam <hossaincrr7@gmail.com>
2020-03-11 05:03:07 +0600

    update community section tablet responsive

--- src/scripts/blocks/community-section.js
+++ src/scripts/blocks/community-section.js
@@ -1,6 +1,10 @@
 import controller from '../util/ScrollMagic'
+import Resposive from '../util/responsive'
+
 
 const communitySction = () => {
+  const communityResposive = new Resposive()
+
   const tl13 = new TimelineMax();
   tl13.to('.o-location', 1, {
     transform: 'translateY(-100%)',
@@ -26,34 +30,59 @@ const communitySction = () => {
   }).setTween(tl13).addTo(controller);
 
   const tl14 = new TimelineMax();
-  tl14.to('.o-community__content', 0.5, {
-    left: 80,
-    top: 60,
-    transform: 'translate(0, 0)',
-    ease: "circ.out",
-  }).to('.o-community__sub', 0.5, {
-    opacity: 1,
-    ease: "circ.out",
-  }, '-=0.5').to('.o-community__image-container-backdrop', 1, {
-    opacity: 1,
-    ease: "circ.out",
-  }, '-=0.5').to('.o-community__image--intro', 0.5, {
-    borderRadius: 0,
-    width: 900,
-    height: 600,
-    transform: 'translate(0, 0)',
-    top: 150,
-    left: 900,
-    ease: "circ.out",
-  }, '-=1').to('.o-community__image--left-1', 0.5, {
-    opacity: 1,
-    scale: 1,
-    ease: "circ.out",
-  }, '-=1').to('.o-community__image--middle-1', 0.5, {
-    opacity: 1,
-    scale: 1,
-    ease: "circ.out",
-  }, '-=1')
+
+  communityResposive.breakpointUp('xl', () => {
+    tl14.to('.o-community__content', 0.5, {
+      left: 80,
+      top: 60,
+      transform: 'translate(0, 0)',
+      ease: "circ.out",
+    }).to('.o-community__sub', 0.5, {
+      opacity: 1,
+      ease: "circ.out",
+    }, '-=0.5').to('.o-community__image-container-backdrop', 1, {
+      opacity: 1,
+      ease: "circ.out",
+    }, '-=0.5').to('.o-community__image--intro', 0.5, {
+      borderRadius: 0,
+      width: 900,
+      height: 600,
+      transform: 'translate(0, 0)',
+      top: 150,
+      left: 900,
+      ease: "circ.out",
+    }, '-=1').to('.o-community__image--left-1', 0.5, {
+      opacity: 1,
+      scale: 1,
+      ease: "circ.out",
+    }, '-=1').to('.o-community__image--middle-1', 0.5, {
+      opacity: 1,
+      scale: 1,
+      ease: "circ.out",
+    }, '-=1')
+  })
+
+  communityResposive.breakpointDown('lg', () => {
+    tl14.to('.o-community__content', 0.5, {
+      left: 30,
+      top: 40,
+      transform: 'translate(0, 0)',
+      ease: "circ.out",
+    }).to('.o-community__sub', 0.5, {
+      opacity: 1,
+      ease: "circ.out",
+    }, '-=0.5').to('.o-community__image-container-backdrop', 1, {
+      opacity: 1,
+      ease: "circ.out",
+    }, '-=0.5').to('.o-community__image--intro', 0.5, {
+      opacity: 0
+    }, '-=1').to('.o-community__image--middle-1', 0.1, {
+      opacity: 1,
+      ease: "circ.out",
+    }, '-=1').to('.o-community__image--clone', 0.1, {
+      opacity: 1
+    }, '-=1')
+  })
 
   const scene14 = new ScrollMagic.Scene({
     triggerElement: "#o-stage-14",
@@ -62,34 +91,64 @@ const communitySction = () => {
 
 
   const tl15 = new TimelineMax()
-  tl15.to('.o-community__image--left-1', 1, {
-    filter: 'blur(2px)',
-    transform: 'translateY(-800px)',
-    ease: "circ.out",
-  }).to('.o-community__image--middle-1', 1, {
-    filter: 'blur(2px)',
-    transform: 'translateY(-1500px)',
-    ease: "circ.out",
-  }, '-=1').to('.o-community__image--right-1', 1, {
-    transform: 'translateY(-800px)',
-    ease: "circ.out",
-  }, '-=1').to('.o-community__image--left-2', 1, {
-    transform: 'translateY(0)',
-    filter: 'blur(0px)',
-    ease: "circ.out",
-  }, '-=1').to('.o-community__image--middle-2', 1.5, {
-    filter: 'blur(2px)',
-    transform: 'translateY(-1500px)',
-    ease: "circ.out",
-  }, '-=1').to('.o-community__image--middle-3', 1, {
-    transform: 'translateY(0px)',
-    filter: 'blur(0px)',
-    ease: "circ.out",
-  }, '-=1.5').to('.o-community__image--right-2', 1, {
-    transform: 'translateY(0)',
-    filter: 'blur(0px)',
-    ease: "circ.out",
-  }, '-=1.5').to('.o-last-page', 0,{display: 'block'})
+
+  communityResposive.breakpointUp('xl', ()=> {
+    tl15.to('.o-community__image--left-1', 1, {
+      filter: 'blur(2px)',
+      transform: 'translateY(-800px)',
+      ease: "circ.out",
+    }).to('.o-community__image--middle-1', 1, {
+      filter: 'blur(2px)',
+      transform: 'translateY(-1500px)',
+      ease: "circ.out",
+    }, '-=1').to('.o-community__image--right-1', 1, {
+      transform: 'translateY(-800px)',
+      ease: "circ.out",
+    }, '-=1').to('.o-community__image--left-2', 1, {
+      transform: 'translateY(0)',
+      filter: 'blur(0px)',
+      ease: "circ.out",
+    }, '-=1').to('.o-community__image--middle-2', 1.5, {
+      filter: 'blur(2px)',
+      transform: 'translateY(-1500px)',
+      ease: "circ.out",
+    }, '-=1').to('.o-community__image--middle-3', 1, {
+      transform: 'translateY(0px)',
+      filter: 'blur(0px)',
+      ease: "circ.out",
+    }, '-=1.5').to('.o-community__image--right-2', 1, {
+      transform: 'translateY(0)',
+      filter: 'blur(0px)',
+      ease: "circ.out",
+    }, '-=1.5').to('.o-last-page', 0, {
+      display: 'block'
+    })
+  })
+
+  communityResposive.breakpointDown('lg', ()=> {
+    tl15.to('.o-community__image--middle-1', 1, {
+      filter: 'blur(2px)',
+      transform: 'translateY(-1500px)',
+      ease: "circ.out",
+    }, '-=1').to('.o-community__image--right-1', 1, {
+      transform: 'translateY(-800px)',
+      ease: "circ.out",
+    }, '-=1').to('.o-community__image--middle-2', 1.5, {
+      filter: 'blur(2px)',
+      transform: 'translateY(-1500px)',
+      ease: "circ.out",
+    }, '-=1').to('.o-community__image--middle-3', 1, {
+      transform: 'translateY(0px)',
+      filter: 'blur(0px)',
+      ease: "circ.out",
+    }, '-=1.5').to('.o-community__image--right-2', 1, {
+      transform: 'translateY(0)',
+      filter: 'blur(0px)',
+      ease: "circ.out",
+    }, '-=1.5').to('.o-last-page', 0, {
+      display: 'block'
+    })
+  })
 
 
   const scene15 = new ScrollMagic.Scene({
@@ -99,34 +158,62 @@ const communitySction = () => {
 
   const tl16 = new TimelineMax();
 
-  tl16.to('.o-community__image--left-2', 1, {
-    filter: 'blur(3px)',
-    transform: 'translateY(-800px)',
-    ease: "circ.out",
-  }).to('.o-community__image--middle-3', 1, {
-    filter: 'blur(5px)',
-    transform: 'translateY(-1500px)',
-    ease: "circ.out",
-  }, '-=1').to('.o-community__image--right-2', 1, {
-    transform: 'translateY(-800px)',
-    ease: "circ.out",
-  }, '-=1').to('.o-community__image--left-3', 1, {
-    transform: 'translateY(0)',
-    filter: 'blur(0px)',
-    ease: "circ.out",
-  }, '-=1').to('.o-community__image--middle-4', 1.5, {
-    filter: 'blur(2px)',
-    transform: 'translateY(-1500px)',
-    ease: "circ.out",
-  }, '-=1').to('.o-community__image--middle-5', 1, {
-    transform: 'translateY(0px)',
-    filter: 'blur(0px)',
-    ease: "circ.out",
-  }, '-=1.5').to('.o-community__image--right-3', 1, {
-    transform: 'translateY(0)',
-    filter: 'blur(0px)',
-    ease: "circ.out",
-  }, '-=1.5')
+
+
+
+  communityResposive.breakpointUp('xl', ()=>{
+    tl16.to('.o-community__image--left-2', 1, {
+      filter: 'blur(3px)',
+      transform: 'translateY(-800px)',
+      ease: "circ.out",
+    }).to('.o-community__image--middle-3', 1, {
+      filter: 'blur(5px)',
+      transform: 'translateY(-1500px)',
+      ease: "circ.out",
+    }, '-=1').to('.o-community__image--right-2', 1, {
+      transform: 'translateY(-800px)',
+      ease: "circ.out",
+    }, '-=1').to('.o-community__image--left-3', 1, {
+      transform: 'translateY(0)',
+      filter: 'blur(0px)',
+      ease: "circ.out",
+    }, '-=1').to('.o-community__image--middle-4', 1.5, {
+      filter: 'blur(2px)',
+      transform: 'translateY(-1500px)',
+      ease: "circ.out",
+    }, '-=1').to('.o-community__image--middle-5', 1, {
+      transform: 'translateY(0px)',
+      filter: 'blur(0px)',
+      ease: "circ.out",
+    }, '-=1.5').to('.o-community__image--right-3', 1, {
+      transform: 'translateY(0)',
+      filter: 'blur(0px)',
+      ease: "circ.out",
+    }, '-=1.5')
+  })
+
+  communityResposive.breakpointDown('lg', ()=>{
+    tl16.to('.o-community__image--middle-3', 1, {
+      filter: 'blur(5px)',
+      transform: 'translateY(-1500px)',
+      ease: "circ.out",
+    }, '-=1').to('.o-community__image--right-2', 1, {
+      transform: 'translateY(-800px)',
+      ease: "circ.out",
+    }, '-=1').to('.o-community__image--middle-4', 1.5, {
+      filter: 'blur(2px)',
+      transform: 'translateY(-1500px)',
+      ease: "circ.out",
+    }, '-=1').to('.o-community__image--middle-5', 1, {
+      transform: 'translateY(0px)',
+      filter: 'blur(0px)',
+      ease: "circ.out",
+    }, '-=1.5').to('.o-community__image--right-3', 1, {
+      transform: 'translateY(0)',
+      filter: 'blur(0px)',
+      ease: "circ.out",
+    }, '-=1.5')
+  })
 
   const scene16 = new ScrollMagic.Scene({
     triggerElement: "#o-stage-16",
